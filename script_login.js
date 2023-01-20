7dee5d74508a37c9302507607e84a9cf949165c9
nunomeira11 <40220240@esmad.ipp.pt>
2023-01-20 15:46:45 +0000

    atualização

--- script_login.js
+++ script_login.js
@@ -14,13 +14,13 @@ function openForm() {
     var $this = $(this),
       $state = $this.find('button > .state');
     $this.addClass('loading');
-    $state.html('Authenticating');
+    $state.html('Autenticação');
     setTimeout(function() {
       $this.addClass('ok');
-      $state.html('Welcome back!');
+      $state.html('Bem-vindo de volta');
       setTimeout(function() {
         $state.html('Log in');
-        $this.removeClass('ok loading');
+        $this.removeClass('loading');
         working = false;
       }, 4000);
     }, 3000);
